8f8c48824ffd84ee4cd81576dc22a1dfc747e6e2
@vortizw <victor.ortiz@incluit.com>
2022-05-14 10:51:49 -0300

    feat: (GREEN) create form

--- academy-front/src/components/Login.tsx
+++ academy-front/src/components/Login.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 export default function Login() {
   return (
-    <div>Login</div>
+<Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Control type="email" placeholder="Email" />
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox" >
+    <Form.Check type="checkbox" label="Remember" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Login
+  </Button>
+</Form>
   )
 }

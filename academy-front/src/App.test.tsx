c6eb7f5ce4cac9b9bcd79f4277550604bfe004e2
@vortizw <victor.ortiz@incluit.com>
2022-05-14 09:28:56 -0300

    refactor: clean code and add logo

--- academy-front/src/App.test.tsx
+++ academy-front/src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe('test login',()=>{
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  test('renders learn react link', () => {
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+})
+

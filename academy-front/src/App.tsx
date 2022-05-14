1ac57b61c79b93d9216c26b4dc3c1e7d6266825b
@vortizw <victor.ortiz@incluit.com>
2022-05-14 10:52:27 -0300

    refactor: add bootstrap and clean code

--- academy-front/src/App.tsx
+++ academy-front/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo my-3" alt="logo" />
         <Login />
       </header>
     </div>

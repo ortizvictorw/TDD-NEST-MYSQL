c3304514becbf00bd65ab1b55ccdb0073291cfa9
@vortizw <victor.ortiz@incluit.com>
2022-05-14 13:20:22 -0300

    test: (RED) StundtService - repository.authLogin - should be defined

--- academy-back/src/student/student.service.spec.ts
+++ academy-back/src/student/student.service.spec.ts
@@ -1,18 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentService } from './student.service';
+import { StudentRepository } from './student-repository.service';
 
 describe('StudentService', () => {
   let service: StudentService;
+  let repository: StudentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [StudentService],
+      providers: [StudentService, StudentRepository],
     }).compile();
 
     service = module.get<StudentService>(StudentService);
+    repository = module.get<StudentRepository>(StudentRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('StundtService - repository.authLogin - should be defined ', () => {
+    expect(repository.authLogin).toBeDefined();
   });
 });

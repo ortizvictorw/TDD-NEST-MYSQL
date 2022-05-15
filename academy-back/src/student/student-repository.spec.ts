0733abb66f30c2c08654eb75b587eddd3252372d
@vortizw <victor.ortiz@incluit.com>
2022-05-14 23:04:41 -0300

    test: (RED) repository - repository.authLogin - should  receive a studentDTO

--- academy-back/src/student/student-repository.spec.ts
+++ academy-back/src/student/student-repository.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { StudentRepository } from './student-repository';
 
 describe('StudentRepository', () => {
-  let provider: StudentRepository;
+  let repository: StudentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [StudentRepository],
     }).compile();
 
-    provider = module.get<StudentRepository>(StudentRepository);
+    repository = module.get<StudentRepository>(StudentRepository);
   });
 
   it('should be defined', () => {
-    expect(provider).toBeDefined();
+    expect(repository).toBeDefined();
   });
 });

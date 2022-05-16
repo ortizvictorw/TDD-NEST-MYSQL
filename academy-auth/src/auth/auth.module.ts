09df701ca7f123625ff2ccd915c5da1802b7405a
@vortizw <victor.ortiz@incluit.com>
2022-05-16 07:27:56 -0300

    refactor: add AuthRepositoryService in module

--- academy-auth/src/auth/auth.module.ts
+++ academy-auth/src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthRepositoryService } from './auth-repository.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, AuthRepositoryService]
 })
 export class AuthModule {}

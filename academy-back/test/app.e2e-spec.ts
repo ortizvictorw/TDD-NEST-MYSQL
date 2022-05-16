7c6863e0635ea6f75f6f95e776a01dcfaa57aa4d
@vortizw <victor.ortiz@incluit.com>
2022-05-15 23:49:09 -0300

    feat: create authModule

--- academy-back/test/app.e2e-spec.ts
+++ academy-back/test/app.e2e-spec.ts
@@ -2,23 +2,27 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { StudentModule } from '../src/student/student.module';
+import { StudentRepository } from '../src/student/student-repository';
+import { studentLoginMock } from '../src/common/mocks';
+jest.mock('../src/student/student-repository');
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, StudentModule],
+      providers: [StudentRepository],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/student/login (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/student/login')
+      .expect(studentLoginMock);
   });
 });

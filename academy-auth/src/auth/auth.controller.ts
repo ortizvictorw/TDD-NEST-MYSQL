892938c0f273e9466298cecb9e9118a916cd9fa0
@vortizw <victor.ortiz@incluit.com>
2022-05-16 06:45:50 -0300

    test: (RED) service.login - should be defined witch studentDto

--- academy-auth/src/auth/auth.controller.ts
+++ academy-auth/src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
 import { Post } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { StudentLoginDto } from './dto/studentLogin.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly auhtService: AuthService) {}
   @Post('/login')
-  login(studentLogin: StudentLoginDto) {}
+  login(studentLogin: StudentLoginDto) {
+    return this.auhtService.login(studentLogin);
+  }
 
   @Post('/register')
   register(studentLogin: StudentLoginDto) {}

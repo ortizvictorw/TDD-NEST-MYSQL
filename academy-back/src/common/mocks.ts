8834ef84df57376b259274fa2911409f621f5528
@vortizw <victor.ortiz@incluit.com>
2022-05-16 11:49:43 -0300

    feat: add mocks

--- academy-back/src/common/mocks.ts
+++ academy-back/src/common/mocks.ts
@@ -1,4 +1,25 @@
+import { EmailDuplicateError } from 'src/student/dto/reponse.dto';
+
 export const studentLoginMock = {
   userName: 'Vcitor Ortiz',
+  email: 'victor.com',
   password: '1234567',
+};
+
+export const studentRegisterMockError = {
+  userName: 'Vcitor Ortiz',
+  email: 'victor.com',
+  password: '1234567',
+};
+
+export const emailDuplicateErrorMock: EmailDuplicateError = {
+  statusCode: 400,
+  message: ['This email is already registered'],
+  error: 'Bad Request',
+};
+
+export const emailInvalidErrorMock: EmailDuplicateError = {
+  statusCode: 400,
+  message: ['email must be an email'],
+  error: 'Bad Request',
 };

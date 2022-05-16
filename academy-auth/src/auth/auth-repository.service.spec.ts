ef736d79a95d608886d435f8563ecfce87148908
@vortizw <victor.ortiz@incluit.com>
2022-05-16 08:50:02 -0300

    test: (RED) repository.register - should be defined witch studentDto

--- academy-auth/src/auth/auth-repository.service.spec.ts
+++ academy-auth/src/auth/auth-repository.service.spec.ts
@@ -1,18 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { studentLoginMock } from '../common/mocks';
 import { AuthRepositoryService } from './auth-repository.service';
 
+jest.mock('./auth-repository.service.ts');
+
 describe('AuthRepositoryService', () => {
-  let service: AuthRepositoryService;
+  let repository: AuthRepositoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthRepositoryService],
+      providers: [AuthRepositoryService, AuthRepositoryService],
     }).compile();
 
-    service = module.get<AuthRepositoryService>(AuthRepositoryService);
+    repository = module.get<AuthRepositoryService>(AuthRepositoryService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  it('repository.register - should be defined witch studentDto', () => {
+    repository.register(studentLoginMock);
+    expect(repository.register).toBeCalledWith(studentLoginMock);
   });
 });

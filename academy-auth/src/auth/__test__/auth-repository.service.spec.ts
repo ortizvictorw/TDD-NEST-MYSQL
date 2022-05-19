d17ccc0785498cf3d279f304e2991d1fe09aed29
@vortizw <victor.ortiz@incluit.com>
2022-05-19 08:37:41 -0300

    test: (RED) repository.login - should be return user

--- academy-auth/src/auth/__test__/auth-repository.service.spec.ts
+++ academy-auth/src/auth/__test__/auth-repository.service.spec.ts
@@ -1,8 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { studentLoginMock, studentRegisterMock } from '../../common/mocks';
+import { studentLoginMock, studentLoginResponseMock } from '../../common/mocks';
 import { AuthRepositoryService } from '../auth-repository.service';
 import { AuthController } from '../auth.controller';
 import { AuthService } from '../auth.service';
+import { StudentLoginDto } from '../dto/studentLogin.dto';
 
 jest.mock('../auth-repository.service.ts');
 
@@ -24,5 +25,16 @@ describe('AuthRepositoryService', () => {
   it('repository.register - should be defined witch studentDto', () => {
     repository.register(studentLoginMock);
     expect(repository.register).toBeCalledWith(studentLoginMock);
+  });
+
+  it('repository.login - should be return user', () => {
+    jest
+      .spyOn(repository, 'login')
+      .mockImplementation((studentLoginMock): StudentLoginDto => {
+        return studentLoginMock;
+      });
+    expect(repository.login(studentLoginMock)).toEqual(
+      studentLoginResponseMock,
+    );
   });
 });
--- academy-auth/src/common/mocks.ts
+++ academy-auth/src/common/mocks.ts
@@ -3,6 +3,18 @@ export const studentLoginMock = {
   email: 'victor@gmail.com',
   password: '1234567',
 };
+export const studentLoginValidateMock = {
+  id: 1,
+  userName: 'Vcitor Ortiz',
+  email: 'victor@gmail.com',
+};
+
+export const studentLoginResponseMock = {
+  userName: 'Vcitor Ortiz',
+  email: 'victor@gmail.com',
+  password: '1234567',
+};
+
 export const studentRegisterMock = {
   id: 1,
   userName: 'Vcitor Ortiz',

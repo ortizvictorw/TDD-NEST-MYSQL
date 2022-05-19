0f2f0f5504de99f78b1778ee6c0b06b5b22f9e71
@vortizw <victor.ortiz@incluit.com>
2022-05-19 09:16:14 -0300

    feat: add validation Pipe

--- academy-auth/src/main.ts
+++ academy-auth/src/main.ts
@@ -1,9 +1,17 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.APP_PORT || 3001);
   Logger.log(`Server is running at ${await app.getUrl()}`);
 }
